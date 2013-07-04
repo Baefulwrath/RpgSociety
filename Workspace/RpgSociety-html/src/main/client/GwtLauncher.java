217bc7f13d054a5566320d39519d61b8eb203942
Oskar Svante Kristensson <oskarsvantek@gmail.com>
2013-07-04 02:30:37 +0200

    Setup
    setup

---
+++ Workspace/RpgSociety-html/src/main/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package main.client;
+
+import main.RPGS;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new RPGS();
+	}
+}
